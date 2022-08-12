2f3e3d45333e0f4150d4101b7341d18662692e31
Tania <taniaportaccio@gmail.com>
2022-08-12 10:37:54 +0100

    h1 changed

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # my-first-website
  
+Hello, I enjoyed discovering new tools such as Bootstrap, different fonts and icons. Playing with CSS is very fun!
+The icons droe me mad, the font awesome ones worked for a while and then suddenly did not, I searched other websides but then their position were not fine and despite styling them they were not responding. I have tried to make the search bar working but I could not, the IndexOf option is not available on Javascript, I do not know why and it gives me an error.
+I still have some issues with Javascript, I hope with time and practice it will be easier. 
+
+I have definitely learnt how to use Bootstrap and play with it. 
--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-// alert( "Hello! thank you for visiting my website!" );
+const search =()=>{
+    const search=document.getElementById("search-item").ariaValueText.toUpperCase;
+    const allProducts= document.getElementById("allHTML");
+    const product= document.querySelectorAll(".allHTML");
+    const pname=document.getElementsByTagName("html");
+
+    for (i=0; i<product.length; i++) {
+        let match = product [i].getElementsByTagName("html")[0];
+    }
+ if (match) { 
+   let textvalue= match.textContent || match.innerHTML
+
+   if (textvalue.toUpperCase()indexOf(search)>-1){
+    product[i].style.display= "";
+   } else {product[i].style.display= "none"
+
+   }
+ }
+}
